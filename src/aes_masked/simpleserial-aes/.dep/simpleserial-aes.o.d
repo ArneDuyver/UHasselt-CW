9f4af1c9f7ade219fe599f64fe3e06c73fd6fbe7
ArneDuyver <arne_duyver@hotmail.com>
2021-05-02 15:59:25 +0200

    added test notebooks for aes_masked and source files for program

---
+++ src/aes_masked/simpleserial-aes/.dep/simpleserial-aes.o.d
@@ -0,0 +1,23 @@
+objdir/simpleserial-aes.o: simpleserial-aes.c \
+ .././crypto/aes-independant.h .././hal/hal.h .././hal/xmega/xmega_hal.h \
+ .././hal/xmega/uart.h .././hal/xmega/usart_driver.h \
+ .././hal/xmega/avr_compiler.h .././hal/xmega/avr_compiler.h \
+ .././simpleserial/simpleserial.h ../crypto/avrcryptolib/aes/aes_sbox.h
+
+.././crypto/aes-independant.h:
+
+.././hal/hal.h:
+
+.././hal/xmega/xmega_hal.h:
+
+.././hal/xmega/uart.h:
+
+.././hal/xmega/usart_driver.h:
+
+.././hal/xmega/avr_compiler.h:
+
+.././hal/xmega/avr_compiler.h:
+
+.././simpleserial/simpleserial.h:
+
+../crypto/avrcryptolib/aes/aes_sbox.h:
---
+++ src/aes_masked/simpleserial-aes/simpleserial-aes.c
@@ -0,0 +1,185 @@
+/*
+    This file is part of the ChipWhisperer Example Targets
+    Copyright (C) 2012-2017 NewAE Technology Inc.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "aes-independant.h"
+#include "hal.h"
+#include "simpleserial.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+//BEGIN EDIT
+    #include "./../crypto/avrcryptolib/aes/aes_sbox.h"
+    #include <avr/pgmspace.h>
+    //aes_sbox.h copy
+    uint8_t aes_sbox2[256];
+    uint8_t * rmask;
+//END EDIT
+
+
+uint8_t get_mask(uint8_t* m, uint8_t len)
+{
+  aes_indep_mask(m, len);
+  return 0x00;
+}
+
+uint8_t get_key(uint8_t* k, uint8_t len)
+{
+	aes_indep_key(k);
+	return 0x00;
+}
+
+uint8_t get_pt(uint8_t* pt, uint8_t len)
+{
+    aes_indep_enc_pretrigger(pt);
+    //START EDIT
+    // generate random mask uint8_t
+    *rmask = 0x55; //rand();
+    uint8_t mask = *rmask;
+    // xor every byte from *pt with random
+    uint8_t i;
+    for(i=0; i<16; ++i){
+        pt[i] ^= mask;
+	}
+    // generate new SBOX using random
+    uint8_t l;
+    for(l=0; l<255; ++l){
+        uint8_t temporary = pgm_read_byte(aes_sbox+l);
+        aes_sbox2[l^mask] = temporary^mask;
+	}   
+    //END EDIT
+
+	trigger_high();
+
+  #ifdef ADD_JITTER
+  for (volatile uint8_t k = 0; k < (*pt & 0x0F); k++);
+  #endif
+
+	aes_indep_enc(pt,aes_sbox2,rmask); /* encrypting the data block */ //ADDED SBOX AND RMASK
+	trigger_low();
+
+    aes_indep_enc_posttrigger(pt);
+    //START EDIT
+    uint8_t j;
+    for(j=0; j<16; ++j){
+        pt[j] ^= mask;
+	}
+    //END EDIT
+
+	simpleserial_put('r', 16, pt);
+	return 0x00;
+}
+
+uint8_t reset(uint8_t* x, uint8_t len)
+{
+    // Reset key here if needed
+	return 0x00;
+}
+
+static uint16_t num_encryption_rounds = 10;
+
+
+uint8_t enc_multi_setnum(uint8_t* t, uint8_t len)
+{
+    //Assumes user entered a number like [0, 200] to mean "200"
+    //which is most sane looking for humans I think
+    num_encryption_rounds = t[1];
+    num_encryption_rounds |= t[0] << 8;
+    return 0;
+}
+
+#if SS_VER == SS_VER_2_0
+uint8_t aes(uint8_t cmd, uint8_t scmd, uint8_t len, uint8_t *buf)
+{
+    uint8_t req_len = 0;
+    uint8_t err = 0;
+    uint8_t mask_len = 0;
+    if (scmd & 0x04) {
+        // Mask has variable length. First byte encodes the length
+        mask_len = buf[req_len];
+        req_len += 1 + mask_len;
+        if (req_len > len) {
+            return SS_ERR_LEN;
+        }
+        err = get_mask(buf + req_len - mask_len, mask_len);
+        if (err)
+            return err;
+    }
+
+    if (scmd & 0x02) {
+        req_len += 16;
+        if (req_len > len) {
+            return SS_ERR_LEN;
+        }
+        err = get_key(buf + req_len - 16, 16);
+        if (err)
+            return err;
+    }
+    if (scmd & 0x01) {
+        req_len += 16;
+        if (req_len > len) {
+            return SS_ERR_LEN;
+        }
+        err = get_pt(buf + req_len - 16, 16);
+        if (err)
+            return err;
+    }
+
+    if (len != req_len) {
+        return SS_ERR_LEN;
+    }
+
+    return 0x00;
+
+}
+#endif
+
+int main(void)
+{
+	uint8_t tmp[KEY_LENGTH] = {DEFAULT_KEY};
+
+    platform_init();
+    init_uart();
+    trigger_setup();
+
+	aes_indep_init();
+	aes_indep_key(tmp);
+
+    /* Uncomment this to get a HELLO message for debug */
+
+/*
+    putch('h');
+    putch('e');
+    putch('l');
+    putch('l');
+    putch('o');
+    putch('\n');
+    */
+
+	simpleserial_init();
+    #if SS_VER == SS_VER_2_0
+    simpleserial_addcmd(0x01, 16, aes);
+    #else
+    simpleserial_addcmd('k', 16, get_key);
+    simpleserial_addcmd('p', 16,  get_pt);
+    simpleserial_addcmd('x',  0,   reset);
+    simpleserial_addcmd_flags('m', 18, get_mask, CMD_FLAG_LEN);
+    simpleserial_addcmd('s', 2, enc_multi_setnum);
+    #endif
+    while(1)
+        simpleserial_get();
+}
